d26409a3bea7d0ba56b3e3b1bdd4824af130d472
Obed Cabanzo <obedc132@gmail.com>
2025-11-07 11:57:10 -0500

    fix: middleware fix

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -4,17 +4,15 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   
-  // Verificar si es el archivo apple-app-site-association
   if (pathname === '/apple-app-site-association' || 
-      pathname === '/.well-known/apple-app-site-association') {
-    
+    pathname === '/.well-known/apple-app-site-association.json') {
+
     const response = NextResponse.next();
     response.headers.set('Content-Type', 'application/json');
     
     return response;
   }
   
-  // Verificar si es el archivo assetlinks.json (Android)
   if (pathname === '/.well-known/assetlinks.json') {
     const response = NextResponse.next();
     response.headers.set('Content-Type', 'application/json');
